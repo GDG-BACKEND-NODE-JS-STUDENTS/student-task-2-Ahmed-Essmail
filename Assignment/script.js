74a48fbbbad23c3cd1afad54a169bbb1be04b08f
Ahmed_Ismail_Ibrahim <ahmedessmail225@gmail.com>
2025-03-09 16:51:39 +0200

    Done

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,1 +1,12 @@
-// Write the codes in here
+var x=5;
+let y="5";
+const c=5;
+
+if(!(x===y) && x==c)// (!(0) && 1 )--> (1 && 1 )--> true
+   {
+     y=Number(y);          // explicit type conversion 
+     ans=y+x+c-"1"-false;  // implicit type conversion  
+   }
+ // ans --> function scope
+ console.log(`Answer is : ${ans} `);// output: Answer is : 14
+ 
